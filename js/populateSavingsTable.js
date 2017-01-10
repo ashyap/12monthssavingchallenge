85d5a61bd8f0819b8593ec1f254bfeaf48117a97
Sashmir Yap <Sashmir Yap>
2017-01-10 20:51:13 +0800

    Initial commit

---
+++ js/populateSavingsTable.js
@@ -0,0 +1,47 @@
+function populateSavingsTable(mode, salary){
+  console.log(mode)
+  var startPercentageSavings = .01;
+  if(mode == "medium"){
+    startPercentageSavings = .05;
+  }
+  else if (mode == "hard") {
+    startPercentageSavings = .09;
+  }
+
+  var tableBody = document.getElementById('savings-content')
+  while (tableBody.hasChildNodes()) {
+    tableBody.removeChild(tableBody.lastChild);
+  }
+
+  var months = new Array("January", "February", "March",
+                         "April", "May", "June",
+                         "July", "August", "September",
+                         "October", "November", "December");
+  var totalSavings = 0;
+  for(var i=0; i < 12; i++){
+    console.log("hello");
+    var tr = document.createElement('TR');
+    var month = months[i];
+    var percentageToSave = (startPercentageSavings + (i/100)).toFixed(2);
+    var savings = (salary * percentageToSave).toFixed(2);
+    totalSavings += parseFloat(savings);
+    var savingsRow = new Array(month, percentageToSave, savings)
+    for (j = 0; j < 3; j++) {
+        var td = document.createElement('TD')
+        td.appendChild(document.createTextNode(savingsRow[j]));
+        tr.appendChild(td);
+        //td.appendChild(document.createTextNode(percentageToSave));
+        //td.appendChild(document.createTextNode(savings));
+    }
+    tableBody.appendChild(tr);
+  }
+
+  var savingsLabel = document.getElementById("savingsLabel")
+  while (savingsLabel.hasChildNodes()) {
+    savingsLabel.removeChild(savingsLabel.lastChild);
+  }
+  console.log(totalSavings);
+  savingsLabel.appendChild(document.createTextNode(totalSavings.toLocaleString()))
+
+
+}
